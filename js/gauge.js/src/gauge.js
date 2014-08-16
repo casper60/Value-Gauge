b3ea51d7f79c259298208fbac18fb11243f1a689
Casper Knudsen <Casper@Caspers-MacBook-Air.local>
2014-08-17 01:28:51 +0200

    Implemented gauge styles and logic

--- js/gauge.js/src/gauge.js
+++ js/gauge.js/src/gauge.js
@@ -1,26 +1,60 @@
 ;(function ($) {
 
 	$.fn.valueGauge = function (params) {
-		var cache = {};
+		var range = { from: -135, to: 135 },
+			ticks = range.to - range.from,
+			element = buildTemplate(params);
 
-		if (!cache[params.id])
-			cache[params.id] = { el: $(this), params: params };
+		// Formats number from 10.000 and up (eg. 10.210 => 10k)
+		function formatNum(num) {
+			num = String(parseInt(num, 10));
+			return num < 10000 ? num : (num / 1000).toFixed(num % 1000 !== 0) + 'k';
+		}
 
-		return this;
+		// Builds css transform object
+		function formatTransformCss(transform) {
+			return { webkitTransform: transform, MozTransform: transform, transform: transform };
+		}
+
+		// Creates a template using jquery to avoid dependencies on other frameworks
+		function buildTemplate(data) {
+			var gaugeWrap = $('<div />', { class: 'valueGauge', id: data.id }),
+				circle = $('<div />', { class: 'gauge' }),
+				needle = $('<div />', { class: 'needle' }),
+				values = $('<div />', { class: 'values' }).text(formatNum(data.value)),
+				title = $('<div />', { class: 'title' }).text(data.title);
+
+			// Apply data values
+			values.attr('data-min', 0).attr('data-max', formatNum(data.valueMax));
+
+			// Merge elements to one object
+			return gaugeWrap.append(circle).append(values).append(needle).append(title);
+		}
+
+		// Updates gauge needle rotation and text values
+		function updateValue(value, valueMax) {
+			var rangeMax = range.to,
+				valueMaxOption = (valueMax || params.valueMax),
+				percentage = value / valueMaxOption,
+				transform = 'rotate(' + Math.min((ticks * percentage) - rangeMax, rangeMax) + 'deg)';
+
+			// Updates needle rotation to reflect progress
+			element.find('.needle').css(formatTransformCss(transform));
+			// Update text values
+			element.find('.values').text(formatNum(value));
+			element.find('.values').attr('data-max', formatNum(valueMaxOption));
+			// Add maxed out animation when valueMax is reached
+			element.one('webkitTransitionEnd transitionend', function () {
+				element.toggleClass('isMaxed', value > valueMaxOption);
+			});
+		}
+
+		// Expose update function on element & append to DOM
+		element.on('updateValue', function (e, value) { updateValue(value); });
+		element.appendTo(this);
+
+		// Wait for element to get inserted into DOM
+		setTimeout(function () { updateValue(params.value, params.valueMax); }, 0);
 	};
 
 }(jQuery));
-
-$('#gaugesWrap').valueGauge({
-	id: 'currentMonthTurnover',
-	title: 'Denne måned',
-	value: 100000,
-	valueMax: 700000
-});
-
-$('#gaugesWrap').valueGauge({
-	id: 'lastMonthTurnover',
-	title: 'Sidste måned',
-	value: 850000,
-	valueMax: 800000
-});
